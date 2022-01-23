d0c907a4794c98930978300a39cec8ab6102d230
Vive Vio Permana <vive.permana@gmail.com>
2022-01-23 19:45:42 +0700

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'sans': 'Inter',
+      'body': 'Inter'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
